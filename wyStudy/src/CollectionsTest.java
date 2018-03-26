79062d8a451d8d5c350f75eac427f70ab49241bb
liuyan <yan.liu@@timanetworks.com>
2018-03-27 00:02:01 +0800

    git commit

---
+++ wyStudy/src/CollectionsTest.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by liuyan on 2018/3/26.
+ */
+public class CollectionsTest {
+    public static void main(String[] strings){
+        ArrayList<String> strings1=new ArrayList<>();
+        strings1.add("r");
+        strings1.remove(0);
+        ;
+    }
+}
